920823a8ce99818707237263b7e7da86933f763d
shadrxcc <akaadesm@gmail.com>
2023-08-24 21:54:04 +0100

    implement hero section

---
+++ src/component/banner/hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import Header from "./header";
+
+const Hero = () => {
+  return (
+    <div>
+      <h1 className="font-bold text-white leading-tight text-center text-2xl md:text-7xl">
+        Igniting Imagination: Witness the Marvels of SpaceX's Spacecraft
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
