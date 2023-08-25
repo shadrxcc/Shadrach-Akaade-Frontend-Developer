0b7736b7ae856753afabe137424e2dbe81d005b1
shadrxcc <akaadesm@gmail.com>
2023-08-25 15:01:51 +0100

    validate proptypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
-import "./App.css";
-import Banner from "./component/banner/banner";
-import RocketsSection from "./component/rockets/rocketssection";
-import "./styles/main.css";
+/* eslint-disable linebreak-style */
+import "./App.css"
+// eslint-disable-next-line linebreak-style
+
+import Banner from "./component/banner/banner"
+
+import RocketsSection from "./component/rockets/rocketssection"
+
+import "./styles/main.css"
+
+import React from "react"
+
+
+
 
 
 function App() {
-  return (
-    <>
-     <Banner/>
-     <RocketsSection/>
-    </>
-  );
+
+	return (
+
+		<>
+
+			<Banner/>
+
+			<RocketsSection/>
+
+		</>
+
+	)
+
 }
 
-export default App;
+
+
+export default App
+
--- src/component/card.jsx
+++ src/component/card.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React from "react"
+import PropTypes from "prop-types"
 
 const Card = (props) => {
-  const classes = "card " + props.className;
-  return (
-    <div onClick={props.onClick} className={classes}>
-      {props.children}
-    </div>
-  );
-};
+	const classes = "card " + props.className
+	return (
+		<div onClick={props.onClick} className={classes}>
+			{props.children}
+		</div>
+	)
+}
 
-export default Card;
+export default Card
+
+Card.propTypes = {
+	className: PropTypes.node,
+	onClick: PropTypes.func,
+	children: PropTypes.node,
+}
